a913c8b80c3dc7cbaaaa48f6dd998ccd5cd848cc
majadda <majadda@malgnsoft.com>
2024-11-27 17:20:25 +0900

    web
    웹사이트 추가

---
+++ leemk/src/main/java/com/leemk/leemk/LeemkApplication.java
@@ -0,0 +1,13 @@
+package com.leemk.leemk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeemkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeemkApplication.class, args);
+	}
+
+}
---
+++ leemk/src/test/java/com/leemk/leemk/LeemkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leemk.leemk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeemkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
